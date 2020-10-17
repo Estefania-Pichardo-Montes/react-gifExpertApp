8b26e43ed771ef9fe65d3bbc1b822ab27cf3470c
Estefania Pichardo <estefania.pichardo@r2devpros.com>
2020-10-17 17:08:13 -0500

    Docs agregado

---
+++ docs/precache-manifest.21a3bbdaa33d6bf0310095d479a44957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d9d4f881460bf4e177dda797df50e48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "874f5db50a4e58a5d654",
+    "url": "/static/css/main.724d9556.chunk.css"
+  },
+  {
+    "revision": "18772ccb525ea2d14384",
+    "url": "/static/js/2.57075067.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.57075067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "874f5db50a4e58a5d654",
+    "url": "/static/js/main.0ee7b3f7.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
